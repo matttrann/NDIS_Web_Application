2bccea1199512b7f2a45b104b799f290627d5b5d
JamieYatesN11241187 <132742618+JamieYatesN11241187@users.noreply.github.com>
2025-04-01 14:54:42 +1000

    version 2
    working TC02 jest test

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jsdom',
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
     '^@/(.*)$': '<rootDir>/$1',
