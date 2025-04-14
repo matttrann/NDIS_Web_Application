b187d3f79264540f213c55b44ee6f4e59da38458
Rezah <mrmaharoof10@gmail.com>
2025-04-14 11:57:03 +1000

    button test

---
+++ __tests__/rezah/mocks/env.mock.js
@@ -0,0 +1,10 @@
+module.exports = {
+    env: {
+        AUTH_SECRET: 'test_auth_secret',
+        GOOGLE_CLIENT_ID: 'test_google_client_id',
+        GOOGLE_CLIENT_SECRET: 'test_google_client_secret',
+        GITHUB_OAUTH_TOKEN: 'test_github_oauth_token',
+        DATABASE_URL: 'test_database_url',
+        RESEND_API_KEY: 'test_resend_api_key',
+    },
+};
